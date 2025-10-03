95dfd672d11bd80c8155c1ed28fa31494447c76e
Ubuntu <ubuntu@ip-172-31-31-245.ec2.internal>
2025-10-03 07:54:16 +0000

    automation change

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
-  it('respond with 404 page not found', function(done) {
+  it('respond it 404 page not found', function(done) {
     request(app)
       .get('/nonexistentpage')
       .expect(404)
